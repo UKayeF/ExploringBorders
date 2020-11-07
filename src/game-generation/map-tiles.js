dc7e0e8c8196dd0c6e2e4e26364d074a7a00ebb3
UKayeF <nilseidmann@gmail.com>
2020-11-07 14:25:09 +0100

    populate space

--- src/game-generation/map-tiles.js
+++ src/game-generation/map-tiles.js
@@ -1,8 +1,19 @@
+const arc_constellation = Array(22).fill('arc_constellation');
+const space_y_constellation = Array(1).fill('space_y_constellation');
+const lambda_constellation = Array(22).fill('lambda_constellation');
+const silky_way = Array(1).fill('silky_way');
+const supernova = Array(4).fill('supernova');
+const green_planet = Array(1).fill('green_planet');
+const red_planet = Array(1).fill('red_planet');
+const solar_system = Array(1).fill('solar_system');
+
 export default [
-  'space_y_constellation',
-  'space_y_constellation',
-  'space_y_constellation',
-  'space_y_constellation',
-  'space_y_constellation',
-  'supernova',
+  ...arc_constellation,
+  ...space_y_constellation,
+  ...lambda_constellation,
+  ...silky_way,
+  ...supernova,
+  ...green_planet,
+  ...red_planet,
+  ...solar_system,
 ]
