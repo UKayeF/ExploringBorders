3a1f26e38e42d538790d9a0ca15d5fe72782c762
UKayeF <nilseidmann@gmail.com>
2020-11-08 15:31:45 +0100

    replace the chain saw with a light sabre cause light sabres rock man!

--- src/components/todos/CompleteChecklist.jsx
+++ src/components/todos/CompleteChecklist.jsx
@@ -30,8 +30,8 @@ const CompleteChecklist = ({ completeTodo }) => {
       <form>
         <input id='email' type='checkbox' onClick={() => setEmailSent(!emailSent)}/>
         <label htmlFor='email'>Send Email to Jeff</label><br/>
-        <input id='chainsaw' type='checkbox' onClick={() => setChainsawed(!chainsawed)}/>
-        <label htmlFor='chainsaw'>Shave beard with chainsaw</label><br/>
+        <input id='lightsabre' type='checkbox' onClick={() => setChainsawed(!chainsawed)}/>
+        <label htmlFor='lightsabre'>Shave beard with light sabre</label><br/>
         <input id='instructions' type='checkbox' onClick={() => setRead(!read)}/>
         <label htmlFor='instructions'>Read instructions properly!</label><br/>
         <button onClick={validateChecklist}>Checklist completed!</button>
